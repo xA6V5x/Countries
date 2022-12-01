21c3e19d8325dec3c9a7972fb957c7cce1a819f7
xA6V5x <ojedaagustinmanuel@gmail.com>
2022-12-01 13:08:44 -0300

    nav y map

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import styles from './Map.module.css';
+import Spinner from '../Spinner/Spinner';
 
 export default function Map(props) {
      const { center = { lat: 44, lng: -80 } } = props;
      const { isLoaded } = useLoadScript({ googleMapsApiKey: '' });
 
-     if (!isLoaded) return <div>Loading</div>;
+     if (!isLoaded) return <Spinner />;
 
      return (
           <div>
